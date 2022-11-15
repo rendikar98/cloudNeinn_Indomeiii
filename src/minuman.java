cd637f5f21fd1aa645d0d14afa80e06ec1d99b04
LailaAsrin <101389349+LailaAsrin@users.noreply.github.com>
2022-11-15 11:01:10 +0700

    Update minuman.java

--- src/minuman.java
+++ src/minuman.java
@@ -1,1 +1,35 @@
+public class minuman extends Produk {
+    public minuman (){
+
+    }
+    @java.lang.Override
+    public double getHarga() {
+        return super.getHarga();
+    }
+
+    @java.lang.Override
+    public String getKode() {
+        return super.getKode();
+    }
+
+    @java.lang.Override
+    public String getNama() {
+        return super.getNama();
+    }
+
+    @java.lang.Override
+    public void setHarga(String harga) {
+        super.setHarga(harga);
+    }
+
+    @java.lang.Override
+    public void setKode(String kode) {
+        super.setKode(kode);
+    }
+
+    @java.lang.Override
+    public void setNama(String nama) {
+        super.setNama(nama);
+    }
+}
 
