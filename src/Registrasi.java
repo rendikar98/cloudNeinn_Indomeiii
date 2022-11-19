638a751cce0c2f22787b8ea37422c24c94740af5
LailaAsrin <101389349+LailaAsrin@users.noreply.github.com>
2022-11-19 12:11:24 +0700

    Create Registrasi.java

---
+++ src/Registrasi.java
@@ -0,0 +1,10 @@
+public class Registrasi extends User{
+    private final String username;
+    private final String password;
+
+    public Registrasi(String email, String password){
+        super(email)
+    }
+
+
+}
