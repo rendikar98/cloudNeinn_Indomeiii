1aff13d844afda5451634056d8e4153e55aeb944
LailaAsrin <101389349+LailaAsrin@users.noreply.github.com>
2022-11-19 17:03:29 +0700

    Update user.java

--- src/user.java
+++ src/user.java
@@ -1,81 +1,64 @@
+package classes;
 import java.util.HashMap;
 
-public class user {
+public class User {
     private String email;
     private String nama;
     private String password;
-    private HashMap<String, makanan> makanan;
-    private HashMap<String, minuman> minuman;
+    private HashMap<String, Kamar> kamar;
 
-public user() {
-    this.nama = null;
-}
+    public User(){
+        this.nama = "Guest";
+    }
 
-public user(String email, String password) {
-    this.nama = "";
-    this.email = email;
-    this.password = password;
-    this.makanan = new HashMap<>();
-    this.minuman = new HashMap<>();
-}
+    public User(String email, String password){
+        this.nama = "";
+        this.password = password;
+        this.email = email;
+        this.kamar = new HashMap<>();
+    }
 
-public String getEmail() {
-    return this.email;
-}
+    public String getEmail() {
+        return this.email;
+    }
 
-public void setEmail(String email) {
-    this.email = email;
-}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-public String getNama() {
-    return this.nama;
-}
+    public String getNama() {
+        return this.nama;
+    }
 
-public void setNama(String nama) {
-    this.nama = nama;
-}
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
 
-public String getPassword() {
-    return this.password;
-}
+    public String getPassword() {
+        return this.password;
+    }
 
-public void setPassword(String password) {
-    this.password = password;
-}
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    public int getJumlahBuku() {
+        return this.kamar.size();
+    }
+    
+    public java.util.Set<String> getKeyBuku() {
+        return this.kamar.keySet();
+    }
 
-public int getJumlahMakanan() {
-    return this.makanan.size();
-}
-
-public int getJumlahMinuman() {
-    return this.minuman.size();
-}
-
-public java.util.Set<String> getKeyMinuman() {
-    return this.minuman.keySet();
-}
-
-public java.util.Set<String> getKeyMakanan() {
-    return this.makanan.keySet();
-}
-
-public Produk getProduk(String kode) {
-    return this.Produk.get(kode);
-}
-
-public void addMakanan(String kode, makanan makanan) {
-    this.makanan.put(kode, makanan);
-}
-
-public void addMinuman(String kode, minuman minuman) {
-    this.minuman.put(kode, minuman);
-}
-
-public void removeMakanan(String kode) {
-    this.makanan.remove(kode);
-}
-
-public void removeMinuman(String kode) {
-    this.minuman.remove(kode);
-}
+    public Kamar getBuku(String kode) {
+        return this.kamar.get(kode);
+    }
+    
+    public void addBuku(String id, Kamar kamar) {
+        this.kamar.put(id, kamar);
+    }
+    
+    public void removeBuku(String kode) {
+        this.kamar.remove(kode);
+    }
 }
