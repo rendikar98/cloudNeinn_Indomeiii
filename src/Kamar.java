3ee71928af999d2d7ac3179f1424b74261cb1c4f
LailaAsrin <101389349+LailaAsrin@users.noreply.github.com>
2022-11-19 12:00:25 +0700

    Create Kamar.java

---
+++ src/Kamar.java
@@ -0,0 +1,8 @@
+public abstract class Kamar {
+    
+    public abstract void Deskripsi();
+    public abstract double getPrice();
+    public abstract void setPrice(String harga);
+    public abstract String getNamaKamar();
+    public abstract void setNamaKamar(String namaKamar);
+}
