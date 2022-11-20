70224907fab110781d759ecabbc0c8b4b70c6cf8
LailaAsrin <101389349+LailaAsrin@users.noreply.github.com>
2022-11-20 13:25:49 +0700

    Update Kamar.java

--- src/Kamar.java
+++ src/Kamar.java
@@ -1,8 +1,26 @@
-public abstract class Kamar {
+public class Kamar {
     
-    public abstract void Deskripsi();
-    public abstract double getPrice();
-    public abstract void setPrice(String harga);
-    public abstract String getNamaKamar();
-    public abstract void setNamaKamar(String namaKamar);
+    private String nama;
+    private int harga;
+
+    public Kamar(String nama, int harga) {
+        this.nama = nama;
+        this.harga = harga;
+    }
+
+    public void setNama( String nama) {
+        this.nama = nama;
+    }
+
+    public void setHarga( int harga) {
+        this.harga = harga;
+    }
+
+    public int getharga(){
+        return this.harga;
+    }
+
+    public String getNama() {
+        return this.nama;
+    }
 }
