d09ba5971a29090127fb2e71319342c1b049bf31
najlarhana <101654857+najlarhana@users.noreply.github.com>
2022-11-21 09:45:05 +0700

    Create KamarSuperior.java
    Membuat subclass dari abstract class

---
+++ src/KamarSuperior.java
@@ -0,0 +1,36 @@
+public class KamarSuperior extends Kamar {
+
+    private double harga;
+    private String deskripsi;
+    private int  kamarTersedia;
+
+    public KamarSuperior(){
+       
+    }
+
+    @Override
+    public double getharga() {
+        return harga;
+    }
+
+    @Override
+    public void setHarga(double hargaBaru) {
+        this.harga = hargaBaru;
+    }
+
+    @Override
+    public String getDeskripsi() {
+       return deskripsi;
+    }
+
+    @Override 
+    public int getKamarTersedia(){
+        return kamarTersedia;
+    }
+
+    @Override 
+    public void setKamarTersedia(int sisaKamar){
+        this.kamarTersedia = sisaKamar;
+    }
+
+}
