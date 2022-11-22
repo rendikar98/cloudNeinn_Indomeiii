6286b5a91f99af8922888629c1ac8a7da1a8eb9f
LailaAsrin <101389349+LailaAsrin@users.noreply.github.com>
2022-11-22 13:37:21 +0700

    Update KamarSuperior.java

--- src/KamarSuperior.java
+++ src/KamarSuperior.java
@@ -1,8 +1,6 @@
 public class KamarSuperior extends Kamar {
 
-    private double harga;
-    private String deskripsi;
-    private int  kamarTersedia;
+    private double harga = 250000;
 
     public KamarSuperior(){
        
@@ -16,20 +14,5 @@ public class KamarSuperior extends Kamar {
     @Override
     public void setHarga(double hargaBaru) {
         this.harga = hargaBaru;
-    }
-
-    @Override
-    public String getDeskripsi() {
-       return deskripsi;
-    }
-
-    @Override 
-    public int getKamarTersedia(){
-        return kamarTersedia;
-    }
-
-    @Override 
-    public void setKamarTersedia(int sisaKamar){
-        this.kamarTersedia = sisaKamar;
     }
 }
