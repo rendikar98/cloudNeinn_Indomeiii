5537d90d62404deff147f071b2deeba0fb3bf2ce
Ardiansyah <101657545+ansyahh@users.noreply.github.com>
2022-11-26 16:27:26 +0700

    Update KamarStandard.java

--- src/KamarStandard.java
+++ src/KamarStandard.java
@@ -1,18 +1,34 @@
+package classes;
+
+@Data  (
+    author = "cloudNein",
+    date = "26/11/2022",
+    description = "Kelas kamarStandard merupakan kelas yang menyimpan variabel dan method dari kamarstandard"
+)
 public class KamarStandard extends Kamar {
-    private double harga = 400000;
-
+    private double harga;
+    
+    /**
+     * method constructor dari kelas kamarstandard yang berisi variabel harga dengan nilai 400000
+     */
     public KamarStandard(){
-       
-    }
-
-    @Override
-    public double getharga() {
-        return harga;
-    }
-
-    @Override
-    public void setHarga(double hargaBaru) {
+        this.harga = 400000;
+     }
+     /**
+      * method yang di override dari superclass kamar untuk mengembalikan harga dari kamar
+      * @return
+      */
+     @Override
+     public double getharga() {
+        return this.harga;
+     }
+     /**
+      * method yang di override dari superclass kamar untuk mengubah harga dari kamr 
+      */
+     @Override
+     public void setHarga(double hargaBaru) {
         this.harga = hargaBaru;
-    }
+     }
+ 
 
 }
