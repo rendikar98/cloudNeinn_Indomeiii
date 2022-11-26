a272563494059b63259601d48b831bea74b6028b
ren9988 <89289250+rendikar98@users.noreply.github.com>
2022-11-26 22:49:39 +0700

    Update KamarSuperior.java

--- src/KamarSuperior.java
+++ src/KamarSuperior.java
@@ -1,35 +1,35 @@
+package src;
+
+@Data  (
+    author = "cloudNein",
+    date = "26/11/2022",
+    description = "Kelas kamarSuperior merupakan kelas yang menyimpan variabel dan method dari kamaarSuperior"
+)
 public class KamarSuperior extends Kamar {
 
     private double harga;
-    private String deskripsi;
-    private int  kamarTersedia;
 
-    public KamarSuperior(){
-       
-    }
+    /**
+     * method constructor dari kelas kamarsuperior yang berisi variabel harga dengan nilai 600000
+     */
+    public KamarDeluxe(){
+        this.harga = 600000;
+     }
+     /**
+      * method yang di override dari superclass kamar untuk mengembalikan harga dari kamar
+      * @return
+      */
+     @Override
+     public double getharga() {
+         return this.harga;
+     }
+     /**
+      * method yang di override dari superclass kamar untuk mengubah harga dari kamr 
+      */
+     @Override
+     public void setHarga(double hargaBaru) {
+         this.harga = hargaBaru;
+     }
+ 
 
-    @Override
-    public double getharga() {
-        return harga;
-    }
-
-    @Override
-    public void setHarga(double hargaBaru) {
-        this.harga = hargaBaru;
-    }
-
-    @Override
-    public String getDeskripsi() {
-       return deskripsi;
-    }
-
-    @Override 
-    public int getKamarTersedia(){
-        return kamarTersedia;
-    }
-
-    @Override 
-    public void setKamarTersedia(int sisaKamar){
-        this.kamarTersedia = sisaKamar;
-    }
 }
