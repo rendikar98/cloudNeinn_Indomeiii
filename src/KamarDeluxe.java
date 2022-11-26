cb3ad966cb2f9b28023759c6cd13d4068adfc80d
Ardiansyah <101657545+ansyahh@users.noreply.github.com>
2022-11-26 16:26:17 +0700

    Update KamarDeluxe.java

--- src/KamarDeluxe.java
+++ src/KamarDeluxe.java
@@ -1,17 +1,32 @@
+package classes;
+
+@Data  (
+    author = "cloudNein",
+    date = "26/11/2022",
+    description = "Kelas kamarDeluxe merupakan kelas yang menyimpan variabel dan method dari kamarDeluxe"
+)
 public class KamarDeluxe extends Kamar {
-    private double harga = 800000;
-
+    private double harga;
+    /**
+     * method constructor dari kelas kamardeluxe yang berisi variabel harga dengan nilai 800000
+     */
     public KamarDeluxe(){
-       
+       this.harga = 800000;
     }
-
+    /**
+     * method yang di override dari superclass kamar untuk mengembalikan harga dari kamar
+     * @return
+     */
     @Override
     public double getharga() {
-        return harga;
+        return this.harga;
     }
-
+    /**
+     * method yang di override dari superclass kamar untuk mengubah harga dari kamr 
+     */
     @Override
     public void setHarga(double hargaBaru) {
         this.harga = hargaBaru;
     }
+
 }
