25915c6e6bdc1f272ff46df533e41bf5827e7499
LailaAsrin <101389349+LailaAsrin@users.noreply.github.com>
2022-11-26 14:30:07 +0700

    Create Data.java

---
+++ src/Data.java
@@ -0,0 +1,14 @@
+package classes;
+
+import java.lang.annotation.Documented;
+/**
+ * kelas ini merupakan kelas annotation yang berisi annotation multivalue
+ */
+
+@Documented
+public @interface Data {
+    String author();
+    String date();
+    String description();
+
+}
