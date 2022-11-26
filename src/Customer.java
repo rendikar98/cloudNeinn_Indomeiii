acac542b1e47d652be7321169f2c9125b5d6aad3
LailaAsrin <101389349+LailaAsrin@users.noreply.github.com>
2022-11-26 14:34:09 +0700

    final
    menambahkan java doc dan annotation pada kelas customer

--- src/Customer.java
+++ src/Customer.java
@@ -1,3 +1,10 @@
+package classes;
+
+@Data  (
+    author = "cloudNein",
+    date = "26/11/2022",
+    description = "Kelas customer merupakan kelas yang menyimpan variabel dan method dari customer"
+)
 public class Customer{
 
     private String nama;
@@ -9,40 +16,47 @@ public class Customer{
         this.id = id;
         this.lamaMenginap = lamaMenginap;
     }
-
+    /**
+     * method untuk mengembalikan nama dari customer
+     * @return nama yang merupakan variabel yang menyimpan nama customer
+     */
     public String getNama() {
         return this.nama;
     }
-
+    /**
+     * method untuk mengubah nama dari customer
+     * @param nama customer
+     */
     public void setNama(String nama) {
         this.nama = nama;
     }
-
+    /**
+     * method untuk mengembalikan id dari customer
+     * @return id
+     */
     public String getId() {
         return this.id;
     }
-
+    /**
+     * method untuk mengubha id customer
+     * @param id
+     */
     public void setId(String id) {
         this.id = id;
     }
-
+    /**
+     * method untuk mengembalikan berapa lama hari menginap dari customer
+     * @return lamamenginap
+     */
     public int getLamaMenginap() {
         return this.lamaMenginap;
     }
-    
+    /**
+     * method untuk mengubah hari nginap dari customer
+     * @param lamaMenginap
+     */
     public void setLamaMenginap(int lamaMenginap) {
         this.lamaMenginap = lamaMenginap;
     }
 
-    public void addkamarStandard() {
-        KamarStandard ks = new KamarStandard();
-    }
-
-    public void addkamarSuperior() {
-        KamarSuperior ksu = new KamarSuperior();
-    }
-
-    public void addkamarDeluxe() {
-        KamarStandard kd = new KamarStandard();
-    }
 }
