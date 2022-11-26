1f66e563f7d7c6c6a56b64911396e5b3a8cb4b0a
ren9988 <89289250+rendikar98@users.noreply.github.com>
2022-11-26 22:46:34 +0700

    Update KamarDeluxe.java

--- src/KamarDeluxe.java
+++ src/KamarDeluxe.java
@@ -1,64 +1,32 @@
-/**
- * Class KamarDeluxe merupakan class turunan dari class Kamar (class abtract).
- * Method di class ini akan meng-override method yang ada di class Kamar
- */
-public class KamarDeluxe extends Kamar {
-    //Inisialisasi variabel
-    private double harga;
-    private String deskripsi;
-    private int  kamarTersedia;
+package src;
 
+@Data  (
+    author = "cloudNein",
+    date = "26/11/2022",
+    description = "Kelas kamarDeluxe merupakan kelas yang menyimpan variabel dan method dari kamarDeluxe"
+)
+public class KamarDeluxe extends Kamar {
+    private double harga;
     /**
-     * Method constructor untuk memberikan harga awal
-     * pada kamar deluxe 
+     * method constructor dari kelas kamardeluxe yang berisi variabel harga dengan nilai 800000
      */
     public KamarDeluxe(){
        this.harga = 800000;
     }
-
     /**
-     * Method accessor untuk return harga kamar 
+     * method yang di override dari superclass kamar untuk mengembalikan harga dari kamar
+     * @return
      */
     @Override
     public double getharga() {
         return this.harga;
     }
-
     /**
-     * Method mutator dengan meng-set harga kamar dengan 
-     * hargaBaru dengan menggunakan parameter hargaBaru
-     * @param hargaBaru
+     * method yang di override dari superclass kamar untuk mengubah harga dari kamr 
      */
     @Override
     public void setHarga(double hargaBaru) {
         this.harga = hargaBaru;
     }
 
-    /**
-     * Method accessor untuk return deskripsi dari kamar 
-     */
-    @Override
-    public String getDeskripsi() {
-       return deskripsi;
-    }
-
-    /**
-     * Method accessor untuk return jumlah kamarTersedia 
-     * pada hotel 
-     */
-    @Override 
-    public int getKamarTersedia(){
-        return kamarTersedia;
-    }
-
-    /**
-     * Method mutator untuk set kamarTersedia dengan parameter
-     * sisaKamar
-     * @param sisaKamar
-     */
-    @Override 
-    public void setKamarTersedia(int sisaKamar){
-        this.kamarTersedia = sisaKamar;
-    }
 }
-
