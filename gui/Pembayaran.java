de10de965c46026ab589c5570f050530c677224d
aliencyy <kelsyamirah62@gmail.com>
2022-11-26 14:14:30 +0700

    final
    menambahkan Pembayaran.java

---
+++ gui/Pembayaran.java
@@ -0,0 +1,25 @@
+package gui;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ * Class Pembayaran merupakan class untuk menampilkan pop up pembayaran customer
+ * @Author CloudNein
+ * @Version 26/11/2022
+ */
+
+public class Pembayaran {
+    
+    /**
+     * fungsi main dari program menampilkan pop up pembayaran dari class Registrasi
+     * 
+     * @param args
+     */
+    public static void main (String args[]){
+        new Registrasi();
+    }
+    
+}
