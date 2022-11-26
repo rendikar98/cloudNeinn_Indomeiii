949daab87d6a02f24db722a255869b38e9a611ee
ren9988 <89289250+rendikar98@users.noreply.github.com>
2022-11-26 14:53:25 +0700

    Update Customer.java

--- src/Customer.java
+++ src/Customer.java
@@ -1,3 +1,9 @@
+@Data  (
+    author = "cloudNein",
+    date = "26/11/2022",
+    description = "Kelas customer merupakan kelas yang menyimpan variabel dan method dari customer"
+)
+
 public class Customer{
 
     private String nama;
@@ -10,26 +16,45 @@ public class Customer{
         this.lamaMenginap = lamaMenginap;
     }
 
+    /**
+     * method untuk mengembalikan nama dari customer
+     * @return nama yang merupakan variabel yang menyimpan nama customer
+     */
     public String getNama() {
         return this.nama;
     }
-
+    /**
+     * method untuk mengubah nama dari customer
+     * @param nama customer
+     */
     public void setNama(String nama) {
         this.nama = nama;
     }
-
+    /**
+     * method untuk mengembalikan id dari customer
+     * @return id
+     */
     public String getId() {
         return this.id;
     }
-
+    /**
+     * method untuk mengubha id customer
+     * @param id
+     */
     public void setId(String id) {
         this.id = id;
     }
-
+    /**
+     * method untuk mengembalikan berapa lama hari menginap dari customer
+     * @return lamamenginap
+     */
     public int getLamaMenginap() {
         return this.lamaMenginap;
     }
-    
+    /**
+     * method untuk mengubah hari nginap dari customer
+     * @param lamaMenginap
+     */
     public void setLamaMenginap(int lamaMenginap) {
         this.lamaMenginap = lamaMenginap;
     }
